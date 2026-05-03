e33738c46730cfd5eea71e086caf701622aae936
atulchakwa <atulchakwa41@gmail.com>
2026-05-03 11:26:10 +0530

    update

--- team-task-manager/server/src/index.js
+++ team-task-manager/server/src/index.js
@@ -19,12 +19,30 @@ connectDB();
 
 app.use(helmet());
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-    ? ['https://ethara-assessment-sage.vercel.app', 'https://ethara-assessment-2wuw37zwu-atulchakwas-projects.vercel.app']
-    : ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5174', 'http://127.0.0.1:5174'],
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    
+    const allowedOrigins = [
+      'https://ethara-assessment-sage.vercel.app',
+      'https://ethara-assessment-2wuw37zwu-atulchakwas-projects.vercel.app',
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'http://localhost:5174',
+      'http://127.0.0.1:5174'
+    ];
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Refresh-Token']
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH', 'HEAD'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Refresh-Token', 'Origin', 'Accept'],
+  exposedHeaders: ['Authorization'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 }));
 app.use(morgan('dev'));
 app.use(express.json({ limit: '10kb' }));
@@ -51,10 +69,23 @@ const server = app.listen(config.PORT, () => {
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.NODE_ENV === 'production' 
-      ? ['https://ethara-assessment-sage.vercel.app', 'https://ethara-assessment-2wuw37zwu-atulchakwas-projects.vercel.app']
-      : ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5174', 'http://127.0.0.1:5174'],
-    credentials: true
+    origin: function(origin, callback) {
+      const allowedOrigins = [
+        'https://ethara-assessment-sage.vercel.app',
+        'https://ethara-assessment-2wuw37zwu-atulchakwas-projects.vercel.app',
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'http://localhost:5174',
+        'http://127.0.0.1:5174'
+      ];
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+    methods: ['GET', 'POST']
   }
 });
 
